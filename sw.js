ced32a64a726aafce84818937ad7c56c9b5bdc47
kimmyungrae80 <77094055+kimmyungrae80@users.noreply.github.com>
2026-04-15 23:07:35 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+// PARKDA PWA 서비스워커
+const CACHE = 'parkda-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+// 설치
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+// 활성화
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// 네트워크 우선, 실패시 캐시
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
